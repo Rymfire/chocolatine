2f9599e828bb18ea67e4be63a413d2933beb3660
Alexis Beaudoin <abeaudoin.dev@gmail.com>
2022-05-22 19:34:42 +0200

    feat(commands): the breakfast commands chooses 2 random persons with a specific role to bring the next breakfasts

--- chocolatine.py
+++ chocolatine.py
@@ -1,18 +1,44 @@
 import os
+import discord
+
 from dotenv import load_dotenv
 from discord.ext import commands
+from random import choice
 
 
 load_dotenv(dotenv_path=".env")
 
 
-class Chocolatine(commands.Bot):
-  def __init__(self):
-    super().__init__(command_prefix="/")
-
-  async def on_ready(self):
-    print("Bot {self.user.display_name} is logged in.")
+intents = discord.Intents.default()
+intents.members = True
+chocolatine = commands.Bot(command_prefix=">", intents=intents)
 
 
-chocolatine = Chocolatine()
+@chocolatine.event
+async def on_ready():
+    print("Bot {0.user.display_name} is logged in.".format(chocolatine))
+
+
+@chocolatine.command(pass_context=True)
+async def breakfast(ctx):
+  members = get_all_guild_members_mentions(ctx.guild)
+  print(members)
+  await ctx.send("This week, **{}** and **{}** will bring us a nice breakfast!\n_PLEASE screenshot this message_".format(choice(members), choice(members)))
+
+
+def get_all_guild_members_mentions(guild):
+  members_mentions = []
+  for member in guild.members:
+    # Add any member's ID that is not the bot ...
+    if member.id != chocolatine.user.id and does_member_has_breakfast_role(member):
+      members_mentions.append(member.mention)
+  return members_mentions
+
+
+def does_member_has_breakfast_role(member):
+  for role in member.roles:
+    if role.name == "Breakfast":
+      return True
+  return False
+
 chocolatine.run(os.getenv("TOKEN"))
